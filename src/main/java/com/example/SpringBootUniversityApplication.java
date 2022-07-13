19693a6246429582361fbad9bcd3a1a89e4495a7
sahithya <Sahithya.annamreddy@gmail.com>
2022-07-13 18:06:04 +0530

    firstcommit

---
+++ src/main/java/com/example/SpringBootUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUniversityApplication.class, args);
+	}
+
+}
